f386052f6868f8c43a186381e7ceebd9311f9e2a
chhatreshkhatri <chhatresh.khatri1002@gmail.com>
2023-11-15 20:22:19 +0530

    custom count start + pass

--- database/mongo.js
+++ database/mongo.js
@@ -1,15 +1,23 @@
-const visitsBadgeSchema = require('./schema.js');
+const visitsBadgeSchema = require("./schema.js");
 
-module.exports.visitsBadge = async function (uniqueID) {
-    const badge = await visitsBadgeSchema.findOneAndUpdate(
-        { uniqueID: uniqueID },
-        {
-            $setOnInsert: {
-                uniqueID: uniqueID,
-            },
-            $inc: { visitsCount: 1 },
-        },
-        { new: true, upsert: true }
-    ).select({ visitsCount: 1 });
-    return badge.visitsCount;
-}
+module.exports.visitsBadge = async function (uniqueID, setIDCount, passKey) {
+  try {
+    const filter = { uniqueID: uniqueID };
+    let existingDocument = await visitsBadgeSchema.findOne({ uniqueID: uniqueID });
+    const update = {
+      $setOnInsert: { uniqueID: uniqueID, passKey: passKey },
+      $set: setIDCount !== "0" && passKey === existingDocument?.passKey ? { visitsCount: parseInt(setIDCount) } : {},
+      $inc: setIDCount === "0" || passKey !== existingDocument?.passKey ? { visitsCount: 1 } : {},
+    };
+
+    const options = { new: true, upsert: true };
+    
+    const updatedDocument = await visitsBadgeSchema.findOneAndUpdate(filter, update, options).select({ visitsCount: 1 });
+
+    return updatedDocument.visitsCount;
+  } catch (error) {
+    console.error("Error in visitsBadge:", error);
+    throw error; 
+  }
+};
+
--- database/schema.js
+++ database/schema.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = mongoose.model("visitsBadge", new mongoose.Schema({
     uniqueID: String,
+    passKey: String,
     visitsCount: {
         type: Number,
         default: 0,
